b1b268ca9425a216a5437a12cf15e95052613c7c
Steve Sinnwell <steve@kopfwelt.com>
2015-10-22 09:11:35 +0200

    [Add] more launcher

--- tests/unit/karma.conf.js
+++ tests/unit/karma.conf.js
@@ -74,7 +74,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome', 'Opera'],
+    browsers: ['Firefox', 'Chrome', 'Opera', 'Safari', 'PhantomJS'],
 
 
     // Continuous Integration mode
